8ce24ed6b3220f59282221e340f703f071132ee9
John Yang <cyan8388@gmail.com>
2024-06-22 15:23:11 +1000

    update(script/covid19_death_icu_curve_script.py)|add(notebooks/covid19_death_icu_curve_plot.ipynb): refactor the code to generate death rate against ICU rate curve and add a notebook for this

--- script/covid19_death_icu_curve_script.py
+++ script/covid19_death_icu_curve_script.py
@@ -1,6 +1,15 @@
 ###############################
 # Import packages
+import git
+repo = git.Repo('.', search_parent_directories=True)
+repo_loc = repo.working_tree_dir
+
+import os
+import sys
 from pathlib import Path
+
+sys.path.append(repo_loc)
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,12 +20,9 @@ import matplotlib.pyplot as plt
 
 # Import custom functions
 # from covid19_prob_func import severe_prob_update, icu_or_vent_prob_update, update_prob, death_num_update
-from covid19_plot_func import plot_death_icu_rate
-from covid19_prob_parameter import P_matrix
-from covid19_model import run_model
-
-
-current_wd = Path(r'C:\John_folder\github_projects\covid19_model')
+from src.covid19_plot_func import plot_death_icu_rate
+from src.covid19_model import run_model, run_multiple
+from src import conf_helper as cf
 ################################
 # Start coding
 
@@ -27,29 +33,23 @@ current_wd = Path(r'C:\John_folder\github_projects\covid19_model')
 # (With the addition of data warehouse in the vacquishcovid19 project, can run with different regions with updated numbers)
 ###########################################################################################################################
 # Bavaria, Lombardy, Wuhan
-from covid19_region_attr import bavaria, lombardy, wuhan, nsw
+from src.covid19_region_attr import create_region
 # Choose a region object to run the model with
+model_config_file = 'model_param_v1.yaml'
+model_config = cf.CovidConf(project_dir=repo_loc, config_file=model_config_file)
 
-list_region = [bavaria, lombardy, wuhan, nsw]
-# 
-
-# n_total_bavaria = bavaria.get_total_case()
-# n_total_wuhan = wuhan.get_total_case()
-# n_total_lombardy = lombardy.get_total_case()
-
-# death_rate_bavaria = bavaria.get_death_rate()*100 # Number obtained from Wiki/Worldometer
-# death_rate_wuhan = wuhan.get_death_rate()*100 # Number obtained from Wiki/Worldometer
-# death_rate_lombardy = lombardy.get_death_rate()*100 # Number obtained from Wiki/Worldometer
-
-# # Bavaria_rate_ade_icu = 4410/3130
-# Bavaria_rate_ade_icu = bavaria.get_est_ade_icu_rate()*100
-# Wuhan_rate_ade_icu = wuhan.get_est_ade_icu_rate()*100
-# Lombardy_rate_ade_icu = lombardy.get_est_ade_icu_rate()*100
+list_region_config = ['bavaria_20200416.yaml', 'lombardy_20200417.yaml', 'nsw_20200425.yaml', 'wuhan_20200412.yaml']
 
 df_d_icu_rate = pd.DataFrame()
 t_hosp_bed = 2000 # Assuming there is enough number of beds
 
-for r in list_region:
+list_region_obj = []
+
+for config_file in list_region_config:
+
+    r = create_region(config_file)
+    
+    list_region_obj.append(r)
     print('Running model with region: ', r.region_name)
     temp_icu_rate = []
 
@@ -60,14 +60,14 @@ for r in list_region:
     t_icu_portion = np.arange(0.0, 1.01, 0.01)
     # t_icu_portion = ""
 
-
-    if t_icu_portion !="":
-        t_icu = (np.array(t_icu_portion)*t_icu).round()
-    else:
+    if t_icu_portion is None:
         t_icu = [t_icu]
-    t_vent = (np.array(t_icu)*0.6).round() # Initial number of ventilators
+    else:
+        t_icu = (np.array(t_icu_portion)*t_icu).round()
+        
 
-    list_df_infected, list_df_death_cause = run_model(r.daily_case, r.pop_ratio, n_days, P_matrix, t_hosp_bed, t_icu, t_vent)
+    t_vent = (np.array(t_icu)*0.6).round() # Initial number of ventilators
+    list_df_infected, list_df_death_cause = run_multiple(r.daily_case, r.pop_ratio, n_days, t_hosp_bed, t_icu, t_vent, model_config)
 
     for i in range(len(list_df_death_cause)):
         temp_icu_rate.append(list_df_death_cause[i]['Total'].sum().sum()/r.get_total_case()*100)
@@ -75,31 +75,7 @@ for r in list_region:
     df_d_icu_rate[r.region_name] = temp_icu_rate
 
 
-# with open('./pickle_file/Wuhan_df_death_cause_100_t_ratios.pkl', 'rb') as f:
-#     list_Wuhan_df_death_cause = pickle.load(f)
-    
-# with open('./pickle_file/Bavaria_df_death_cause_100_t_ratios.pkl', 'rb') as f:
-#     list_Bavaria_df_death_cause = pickle.load(f)
-    
-# with open('./pickle_file/Lombardy_df_death_cause_100_t_ratios.pkl', 'rb') as f:
-#     list_Lombardy_df_death_cause = pickle.load(f)
-
-# Wuhan_d_icu_rate = []
-# Bavaria_d_icu_rate = []
-# Lombardy_d_icu_rate= []
-
-# for i in range(len(list_Wuhan_df_death_cause)):
-#     Wuhan_d_icu_rate.append(list_Wuhan_df_death_cause[i]['Total'].sum().sum()/n_total_wuhan*100)
-#     Bavaria_d_icu_rate.append(list_Bavaria_df_death_cause[i]['Total'].sum().sum()/n_total_bavaria*100)
-#     Lombardy_d_icu_rate.append(list_Lombardy_df_death_cause[i]['Total'].sum().sum()/n_total_lombardy*100)
-    
-# # death_against_icu_rate[100] = 
-# # print(Wuhan_d_icu_rate)
-
-# df_d_icu_rate = pd.DataFrame(np.array([Bavaria_d_icu_rate, Wuhan_d_icu_rate, Lombardy_d_icu_rate]).T, \
-#                              columns=['Bavaria', 'Wuhan', 'Lombardy'])
-
-ax = plot_death_icu_rate(df_d_icu_rate, list_region)
+ax = plot_death_icu_rate(df_d_icu_rate, list_region_obj)
 
 # Bavaria_60_proportion = (0.11751394 + 0.08948367 + 0.0600025)*100
 # Wu_60_proportion = (0.073404958 + 0.039966121 + 0.013451689)*100
