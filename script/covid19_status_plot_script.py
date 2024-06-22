92b21b22a5d8a4279f08b9ca1e905ba94a433b12
John Yang <cyan8388@gmail.com>
2024-06-22 21:40:08 +1000

    update(notebooks/covid19_main_model_plot.ipynb|script/covid19_status_plot_script.py): refactor and clean the code

--- script/covid19_status_plot_script.py
+++ script/covid19_status_plot_script.py
@@ -1,35 +1,33 @@
 ###############################
 # Import packages
+import git
+repo = git.Repo('.', search_parent_directories=True)
+repo_loc = repo.working_tree_dir
+
+import os
+import sys
 from pathlib import Path
+
+sys.path.append(repo_loc)
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-# import sklearn
-# import requests
-# from bs4 import BeautifulSoup
-# import pickle
 
 # Import custom functions
-# from covid19_prob_func import severe_prob_update, icu_or_vent_prob_update, update_prob, death_num_update
-from src.covid19_plot_func import plot_state_num, plot_death_cause, plot_death_cumsum, plot_death_icu_rate
-from src.covid19_prob_parameter import P_matrix
-from src.covid19_model import run_model
-
-
-current_wd = Path(r'C:\John_folder\github_projects\covid19_model')
+from src.covid19_model import run_model, run_multiple
+from src import conf_helper as cf
+from src.covid19_region_attr import create_region
+from src import covid19_plot_func
 ################################
 # Start coding
 
-###########################################################################################################################
-# Loading Covid-19 case data
-# Choose a region to run the model
-# (Thus far I have outputed only Bavaria (Germany), Lombardy (Italy) and Wuhan (China) Excel for running the model
-# (With the addition of data warehouse in the vacquishcovid19 project, can run with different regions with updated numbers)
-###########################################################################################################################
-# Bavaria, Lombardy, Wuhan
-from src.covid19_region_attr import bavaria, lombardy, wuhan, nsw
+model_config_file = 'model_param_v1.yaml'
+model_config = cf.CovidConf(project_dir=repo_loc, config_file=model_config_file)
+
 # Choose a region object to run the model with
-region = lombardy
+region_config = 'bavaria_20200416.yaml' # 'lombardy_20200417.yaml', 'nsw_20200425.yaml', 'wuhan_20200412.yaml']
+
+region = create_region(region_config)
 
 n_days = len(region.daily_case) + 50
 
@@ -57,26 +55,22 @@ t_vent = (np.array(t_icu)*0.6).round() # Initial number of ventilators
 # Run model
 ############
 print("Run model for region: ", region.region_name)
-list_df_infected, list_df_death_cause = run_model(region.daily_case, region.pop_ratio, n_days, P_matrix, t_hosp_bed, t_icu, t_vent)
-
-
+list_df_infected, list_df_death_cause = run_multiple(region.daily_case, region.pop_ratio, n_days, t_hosp_bed, t_icu, t_vent, model_config)
 
 ################
 # Plotting part
 ################
-
 df_infected = list_df_infected[0]
 df_death_cause = list_df_death_cause[0]
 
-
 ##### Number of patients in different state plot (plot_state_num) #####
-plot_state_num(df_infected, region.daily_case.sum(), region.get_plot_title('plot_state_num', t_icu[0]))
+covid19_plot_func.plot_state_num(df_infected, region.daily_case.sum(), region.get_plot_title('plot_state_num', t_icu[0]))
 
 ##### Number of deaths due to different causes over time #####
-plot_death_cause(df_death_cause, region.daily_case.sum(), region.get_plot_title('plot_death_num', t_icu[0]))
+covid19_plot_func.plot_death_cause(df_death_cause, region.daily_case.sum(), region.get_plot_title('plot_death_num', t_icu[0]))
 
 ##### Accumulated number of deaths due to different cuases over time #####
-plot_death_cumsum(df_death_cause, region.daily_case.sum(), region.get_plot_title('plot_death_cumsum', t_icu[0]))
+covid19_plot_func.plot_death_cumsum(df_death_cause, region.daily_case.sum(), region.get_plot_title('plot_death_cumsum', t_icu[0]))
 
 
 
