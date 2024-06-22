f56f1740cc0eb39509634d00bcbee7bc81802b53
John Yang <cyan8388@gmail.com>
2024-06-22 21:13:34 +1000

    add(notebooks/covid19_icu_patient_trend_fixed_inflow.ipynb)|update(script/covid19_icu_patient_trend_fixed_inflow_script.py): refactor the code to plot fixed inflow number of Covid cases and add a notebook for it

--- script/covid19_icu_patient_trend_fixed_inflow_script.py
+++ script/covid19_icu_patient_trend_fixed_inflow_script.py
@@ -7,31 +7,35 @@ Created on Wed May  5 00:29:57 2021
 
 ###############################
 # Import packages
+import git
+repo = git.Repo('.', search_parent_directories=True)
+repo_loc = repo.working_tree_dir
+
+import os
+import sys
 from pathlib import Path
+
+sys.path.append(repo_loc)
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-# import sklearn
-# import requests
-# from bs4 import BeautifulSoup
-# import pickle
 
 # Import custom functions
 # from covid19_prob_func import severe_prob_update, icu_or_vent_prob_update, update_prob, death_num_update
-from covid19_plot_func import plot_constant_daily_case_curve
-from covid19_prob_parameter import P_matrix
-from covid19_model import run_model
-
-current_wd = Path(r'C:\John_folder\github_projects\covid19_model')
+from src.covid19_plot_func import plot_death_icu_rate
+from src.covid19_model import run_model, run_multiple
+from src import conf_helper as cf
+from src.covid19_region_attr import create_region
+from src import covid19_plot_func
 ################################
 
-from covid19_region_attr import bavaria, lombardy, wuhan, nsw
+model_config_file = 'model_param_v1.yaml'
+model_config = cf.CovidConf(project_dir=repo_loc, config_file=model_config_file)
+
 # Choose a region object to run the model with
-r = wuhan
+region_config = 'bavaria_20200416.yaml' # 'lombardy_20200417.yaml', 'nsw_20200425.yaml', 'wuhan_20200412.yaml']
 
-# with open('./pickle_file/constant_infect_num_df_infected_NSW.pkl', 'rb') as f:
-#     list_constant_df_infected = pickle.load(f)
-
+r = create_region(region_config)
 
 list_base_factor = [1, 2, 5, 10, 20]
 
@@ -57,23 +61,22 @@ for factor in list_base_factor:
     constant_daily_case = n_base_test_case*factor
     daily_case = [constant_daily_case]*constant_flow_days # constant daily cases for 30 days
     
-    constant_flow_text.append(f'{constant_daily_case} per day ({"%.2f" % ((constant_daily_case/r.get_total_pop())*100000)} per 100K pop per day)')
+    constant_flow_text.append(f'{constant_daily_case} per day ({"%.2f" % ((constant_daily_case/r.get_total_pop())*100000)} per 100K pop per day)')    
     
-    
-    list_df_infected, list_df_death_cause = run_model(daily_case, r.pop_ratio, n_days, P_matrix, t_hosp_bed, t_icu, t_vent)
+    list_df_infected, list_df_death_cause = run_multiple(daily_case, r.pop_ratio, n_days, t_hosp_bed, t_icu, t_vent, model_config)
     
     list_constant_df_infected.append(list_df_infected[0])
     
 icu_case_max = (list_df_infected[0]['Total']['ICU'].max() + list_df_infected[0]['Total']['ICU + ventilator'].max())
 
 # Run the daily case from the region as well
-list_df_infected, list_df_death_cause = run_model(r.daily_case, r.pop_ratio, n_days, P_matrix, t_hosp_bed, t_icu, t_vent)
+list_df_infected, list_df_death_cause = run_multiple(r.daily_case, r.pop_ratio, n_days, t_hosp_bed, t_icu, t_vent, model_config)
     
 list_constant_df_infected.append(list_df_infected[0])
 constant_flow_text.append(f'Inflow with {r.region_name} cases')
 
 
-plot_constant_daily_case_curve(r, list_constant_df_infected, constant_flow_text, icu_case_max)
+covid19_plot_func.plot_constant_daily_case_curve(r, list_constant_df_infected, constant_flow_text, icu_case_max)
 
 
 # constant_flow_text = [f'{n_base_test_case} per day ({"%.2f" % ((n_base_test_case/r.get_total_pop())*100000)} per 100K pop per day)', \
